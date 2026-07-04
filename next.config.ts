ac400da41ea3ad5c71fb373d11750586bb58a8c9
braderdin <braderdin006@gmail.com>
2026-07-05 06:41:36 +0800

    feat: downgrade to hobby grade and bypass strict type checking checks

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* Start: Hobby Grade Relax Mode Build Configurations */
+  
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has TypeScript type compilation errors.
+    ignoreBuildErrors: true,
+  },
+  
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint validation errors.
+    ignoreDuringBuilds: true,
+  },
+
+  /* End: Hobby Grade Relax Mode Build Configurations */
 };
 
 export default nextConfig;
